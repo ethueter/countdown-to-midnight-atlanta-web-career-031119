a6a1d25edfda179a68926767314dfead39fd21a8
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-25 18:14:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,8 @@
 
 def countdown(seconds)
   while seconds > 0 
-  puts " #{seconds} SECOND (S)!"
+  puts " #{seconds} SECOND(S)!"
   seconds -= 1 
 end
-puts "HAPPY NOEW YEAR!"
+puts "HAPPY NEW YEAR!"
 end
